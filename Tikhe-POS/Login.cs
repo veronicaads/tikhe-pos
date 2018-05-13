b9f1205bc07771050d381214d43d7749808e8dab
Veronica <Veronica@VERONICA-PC>
2018-05-13 21:29:20 +0700

    login

--- Tikhe-POS/Login.cs
+++ Tikhe-POS/Login.cs
@@ -10,11 +10,17 @@ using System.Windows.Forms;
 
 namespace Tikhe_POS
 {
+    
     public partial class Login : Form
     {
+        
         public Login()
         {
             InitializeComponent();
+            // Set to no text.  
+            password_edit.Text = "";
+            // The password character is an asterisk.  
+            password_edit.PasswordChar = '*';
         }
 
         private void Login_Load(object sender, EventArgs e)
@@ -29,8 +35,18 @@ namespace Tikhe_POS
 
         private void login_btn_Click(object sender, EventArgs e)
         {
-            MainMenu form = new MainMenu();
-            form.Show(); Hide();
+            String username = username_edit.Text;
+            String password = password_edit.Text;
+            if (String.Compare(username,"superadmin")==0 && String.Compare(password,"shoebox555")==0)
+            {
+                MainMenu form = new MainMenu();
+                form.Show(); Hide();
+            }
+            else
+            {
+                MessageBox.Show("Username atau Password tidak sesuai. Silahkan ulangi!");
+
+            }
         }
 
         private void button3_Click(object sender, EventArgs e)
